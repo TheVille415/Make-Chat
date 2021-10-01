18a5e251a834ba1befd16f039e57553606e11bb5
Jordan_Torres <jt.arontorres@gmail.com>
2021-10-01 00:47:23 -0700

    Users can join chat and send messages

--- sockets/chat.js
+++ sockets/chat.js
@@ -9,6 +9,11 @@ module.exports = (io, socket) => {
         // io.emit sends data to all clients on the connection.        
         io.emit("new user", username);
       })
-      
+    //Listen for new messages
+    socket.on('new message', (data) => {
+        // Send that data back to ALL clients
+        console.log(`🎤 ${data.sender}: ${data.message} 🎤`)
+        io.emit('new message', data);
+    })
   
   }
